3e18c5f8f8f58ccd06a2fca32e39aa657c18a3ca
Drewflores <andrewflores303641@gmail.com>
2026-03-13 15:11:37 +0800

    moved the game from console to ui

--- function.js
+++ function.js
@@ -1,5 +1,50 @@
 let humanScore = 0;
 let computerScore = 0;
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const result = document.getElementById("result");
+const output1 = document.getElementById("outputOne");
+const output2 = document.getElementById("outputTwo");
+
+rock.addEventListener("click", () => {
+    playRound("ROCK");
+
+    while (humanScore < 3 && computerScore < 3){
+        output2.textContent = "Next Round!";
+    }
+    if (humanScore == 3){
+        output2.textContent = "You win!";
+    } else {
+        output2.textContent = "You Lose!";
+    }
+});
+
+paper.addEventListener("click", () => {
+    playRound("PAPER");
+
+    while (humanScore < 3 && computerScore < 3){
+        output2.textContent = "Next Round!";
+    }
+    if (humanScore == 3){
+        output2.textContent = "You win!";
+    } else {
+        output2.textContent = "You Lose!";
+    }
+});
+
+scissors.addEventListener("click", () => {
+    playRound("SCISSORS");
+
+    while (humanScore < 3 && computerScore < 3){
+        output2.textContent = "Next Round!";
+    }
+    if (humanScore == 3){
+        output2.textContent = "You win!";
+    } else {
+        output2.textContent = "You Lose!";
+    }
+});
 
 const getComputerChoice = () => {
     const num = Math.floor(Math.random() * 10);
@@ -12,52 +57,33 @@ const getComputerChoice = () => {
     }
 };
 
-const getHumanChoice = () => {
-    const choice = prompt("Select one from the 3 choices(rock, paper, scissors)");
-    return choice;
-};
-
-const playRound = () => {
-    const humChoice = getHumanChoice().toUpperCase();
+const playRound = (move) => {
+    const humChoice = move;
     const comChoice = getComputerChoice();
 
     if (humChoice === comChoice){
-        console.log(`No Winner: ${humChoice} is equal to ${comChoice}`);
+        output1.textContent += `No Winner: ${humChoice} is equal to ${comChoice}`;
     } else if (humChoice === "ROCK" && comChoice === "SCISSORS"){
         humanScore += 1;
-        console.log("You Win! Rock beats Scissors");
+        output1.textContent += "You Win! Rock beats Scissors";
     } else if (humChoice === "PAPER" && comChoice === "ROCK"){
         humanScore += 1;
-        console.log("You Win! Paper beats Rock");
+        output1.textContent += "You Win! Paper beats Rock";
     } else if (humChoice === "SCISSORS" && comChoice === "PAPER"){
         humanScore += 1;
-        console.log("You Win! Scissors beats Paper");
+        output1.textContent += "You Win! Scissors beats Paper";
     } else if (comChoice === "ROCK" && humChoice === "SCISSORS"){
         computerScore += 1;
-        console.log("You Lose! Rock beats Scissors");
+        output1.textContent += "You Lose! Rock beats Scissors";
     } else if (comChoice === "PAPER" && humChoice === "ROCK"){
         computerScore += 1;
-        console.log("You Lose! Paper beats Rock");
+        output1.textContent += "You Lose! Paper beats Rock";
     } else if (comChoice === "SCISSORS" && humChoice === "PAPER"){
         computerScore += 1;
-        console.log("You Lose! Scissors beats Paper");
+        output1.textContent += "You Lose! Scissors beats Paper";
     } else {
         computerScore += 1;
-        console.log(`You Lose! ${humChoice} is invalid`);
+        output1.textContent += `You Lose! ${humChoice} is invalid`;
     }
 };
 
-const playGame = () => {
-    
-    do {
-        playRound();
-    } while (humanScore < 3 && computerScore < 3);
-
-    if (humanScore == 3){
-        return `You Win! ${humanScore} : ${computerScore}`;
-    } else {
-        return `You Lose! ${computerScore} : ${humanScore}`;
-    }
-};
-
-console.log(playGame());
